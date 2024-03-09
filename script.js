d74453f6e44ace007eb94f19444f002ce6cd6682
igorsemenovdrums <igorsemenov@gmx.com>
2024-03-09 15:02:43 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const finger = Number(prompt());
+if (finger === 1) {
+    alert("Thumb");
+}
+else if (finger === 2) {
+    alert("Index");
+}
+else if (finger === 3) {
+    alert("Middle");
+}
+else if (finger === 4) {
+    alert("Ring");
+}
+else if (finger === 5) {
+    alert("Pincky");
+}
